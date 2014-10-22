d459db0dac99b50d27bcd1773da952659425f683
Landon Fowles <landon@lfowles.org>
2014-10-22 00:34:45 -0500

    Forgot to check in completely essential file for Issue #31 server auth

---
+++ freecell/server/loginserver.py
@@ -0,0 +1,53 @@
+import hashlib
+import random
+import string
+
+from events import event_dispatch, AuthEvent
+
+from ..shared.version import VERSION
+
+USER_DATABASE = {}
+
+class LoginWrapper(object):
+    def __init__(self, connection):
+        self.connection = connection
+        self.event_dispatch = event_dispatch
+        self.nonce = random.randint(0, 0xFFFFFFFF)
+
+        self.event_dispatch.register(self.login, ["LoginEvent"])
+        self.event_dispatch.register(self.register, ["RegisterEvent"])
+
+    # RegisterEvent(username)
+    def register(self, event):
+        if event.id == self.connection.id:
+            if event.username in USER_DATABASE:
+                self.connection.send_json({"event":"nametaken", "username":event.username})
+            else:
+                salt = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(16))
+                token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(16))
+                USER_DATABASE[event.username] = (hashlib.sha256(token+salt).hexdigest(), salt)
+                self.connection.send_json({"event":"logintoken", "username":event.username, "token": token})
+
+    # LoginEvent(username)
+    def login(self, event):
+        if event.id == self.connection.id:
+            if event.username in USER_DATABASE:
+                self.connection.send_json({"event":"nonce", "nonce":self.nonce, "salt":USER_DATABASE[event.username][1]})
+                self.event_dispatch.register(self.response, ["TokenHashEvent"])
+            else:
+                self.connection.send_json({"event":"unknownuser", "username":event.username})
+
+    # TokenHashEvent(username, nonce_hash)
+    def response(self, event):
+        if event.id == self.connection.id and event.username in USER_DATABASE:
+            nonce_hash = hashlib.sha256(USER_DATABASE[event.username][0]+str(self.nonce)).hexdigest()
+            if nonce_hash == event.nonce_hash:
+                self.connection.send_json({"event":"loggedin", "username":event.username})
+                self.event_dispatch.send(AuthEvent(id=self.connection.id, connection=self.connection))
+            else:
+                self.connection.send_json({"event":"loginfailed", "username":event.username})
+            self.event_dispatch.unregister(self.response, ["TokenHashEvent"])
+
+class LoginServer(object):
+    def __init__(self):
+        pass

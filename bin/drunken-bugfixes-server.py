76ea1a49575ab31c4b5772727ce5e41c70f1e8fe
Landon Fowles <landon@lfowles.org>
2014-10-21 21:02:41 -0500

    Server can take a "debug" argument

--- bin/drunken-bugfixes-server.py
+++ bin/drunken-bugfixes-server.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python2.7
+import sys
 HOST, PORT = "localhost", 11982
 
 from freecell.server.competitionserver import CompetitionServer
 
 if __name__ == "__main__":
+    debug = False
+    if len(sys.argv) > 1:
+        if "debug" in sys.argv:
+            sys.argv.remove("debug")
+            debug = True
+        from pydevd import pydevd
+        from debug import DEBUG_HOST, DEBUG_PORT
+        pydevd.settrace(DEBUG_HOST, port=DEBUG_PORT, suspend=False)
     competition_server = CompetitionServer(HOST, PORT)
     competition_server.start()
